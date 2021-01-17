e77f910d4d8582d2615927533043ff509c62b585
Luis Gomez <luis.g.1990@gmail.com>
2021-01-16 21:39:07 -0600

    Added solution for part 1 (PyBank)

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,91 @@
-print("Hello from PyBank!")
+# PyBank Assignment
+# By: Luis Gomez - TTh 
+
+# Dependencies
+
+import os
+import csv
+
+# Read file
+
+csv_path = os.path.join("Resources", "budget_data.csv")
+
+# Open CSV file
+
+with open(csv_path) as csvfile:
+
+    csvreader = csv.reader(csvfile, delimiter = ",")
+
+    # Initialize variables to store data of interest
+
+    total_months = 0
+    total_revenue = int()
+    avg_revenue = float()
+    max_date = ""
+    max_profit = 0
+    min_date = ""
+    min_profit = 0
+
+    # Skip header row
+
+    header_row = next(csvreader)
+
+    # For loop to read through the data and collect variables
+
+    for row in csvreader:
+        
+        # Define variables
+
+        date = row[0]
+        profit = int(row[1])
+
+        # Totalize months over dataset and profit
+
+        total_months += 1
+        total_revenue += profit
+
+        # Conditionals to compare datapoints and determine min/max profit and store date/profit in variables
+
+        # Max profit
+
+        if profit > max_profit: 
+            max_date = date
+            max_profit = profit 
+        
+        # Min profit
+        
+        elif profit < min_profit:
+            min_date = date
+            min_profit = profit
+
+
+    # Calculate average revenue, dividing total revenue by number of entries
+
+    avg_revenue = round(total_revenue / total_months, 2)
+
+# Now that we have all of our variables, we can print them out... 
+
+print("Financial Analysis")
+print("----------------------------")
+print(f"Total Months: {total_months}")
+print(f"Total: ${total_revenue}")
+print(f"Average Change: ${avg_revenue}")
+print(f"Greatest Increase in Profits: {max_date} (${max_profit})")
+print(f"Greatest Decrease in Profits: {min_date} (${min_profit})")
+
+# ...and store them in a .txt file
+
+with open("Output/bank_summary.txt", "w") as text:
+    print("Financial Analysis", file = text)
+    print("----------------------------", file = text)
+    print(f"Total Months: {total_months}", file = text)
+    print(f"Total: ${total_revenue}", file = text)
+    print(f"Average Change: ${avg_revenue}", file = text)
+    print(f"Greatest Increase in Profits: {max_date} (${max_profit})", file = text)
+    print(f"Greatest Decrease in Profits: {min_date} (${min_profit})", file = text)
+
+
+        
+
+
+
